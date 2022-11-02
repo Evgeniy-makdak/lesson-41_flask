a294c8682a55725913821604dd272b990a8b74b6
Evgeniy Acteck <makdak23@mail.ru>
2022-11-02 21:07:39 +0300

    0.0.2 add screenshots

--- Динамические_маршруты/example.py
+++ Динамические_маршруты/example.py
@@ -4,21 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Welcome to Skypro!'
-
-
-@app.get('/users')
-def users_get():
-    return 'GET /users'
-
-
-@app.post('/users')
-def users():
-    return 'Users', 302
-
-
 @app.route('/courses/<id>')
 def courses(id):
     return f'Course id: {id}'
+
+
+app.run()
--- Обработчик_запросов/example.py
+++ Обработчик_запросов/example.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 # callable WSGI-приложение
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Welcome to Skypro!'
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    # Получить доступ к содержимому запроса можно через специальный объект request
+    if request.method == 'POST':
+        return 'Hello, POST!'
+    return 'Hello, GET!'
 
 
 @app.get('/users')
@@ -17,3 +20,6 @@ def users_get():
 @app.post('/users')
 def users_post():
     return 'POST /users'
+
+
+app.run(debug=True)
